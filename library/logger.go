f6015f64ffd7fac19e057802dd9ed313e6294df9
farhaniupr <farhaniupr@gmail.com>
2023-11-30 10:06:22 +0700

    write log without context

--- library/logger.go
+++ library/logger.go
@@ -81,4 +81,55 @@ func Writelog(c echo.Context, env Env, logLevel string, msg string) {
 
 }
 
-// library.Writelog(c, u.env, "debug", "created")
+func WritelogWithoutContext(env Env, logLevel string, msg string) {
+
+	atomicLevel := zap.NewAtomicLevel()
+	switch logLevel {
+	case "debug":
+		atomicLevel.SetLevel(zapcore.DebugLevel)
+	case "info":
+		atomicLevel.SetLevel(zapcore.InfoLevel)
+	case "warn":
+		atomicLevel.SetLevel(zapcore.WarnLevel)
+	case "err":
+		atomicLevel.SetLevel(zapcore.ErrorLevel)
+	}
+
+	endoderConfig := zapcore.EncoderConfig{
+		TimeKey:        "time",
+		LevelKey:       "level",
+		NameKey:        "name",
+		CallerKey:      "line",
+		MessageKey:     "msg",
+		FunctionKey:    "func",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,
+		EncodeTime:     zapcore.ISO8601TimeEncoder,
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeCaller:   zapcore.FullCallerEncoder,
+		EncodeName:     zapcore.FullNameEncoder,
+	}
+
+	writer := &lumberjack.Logger{
+		Filename:   env.LogOutput,
+		MaxSize:    100, //100mb
+		MaxAge:     30,
+		MaxBackups: 5, //5compress
+		LocalTime:  true,
+		Compress:   true,
+	}
+
+	zapCore := zapcore.NewCore(
+		zapcore.NewConsoleEncoder(endoderConfig),
+		zapcore.AddSync(writer),
+		atomicLevel,
+	)
+
+	logger := zap.New(zapCore, zap.AddCaller(), zap.AddStacktrace(zapcore.ErrorLevel))
+
+	logger.Log(atomicLevel.Level(), msg)
+
+	defer logger.Sync()
+
+}
